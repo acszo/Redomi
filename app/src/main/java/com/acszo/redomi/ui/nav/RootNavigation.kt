fdbb39941a7919b18d1708b53d10a8a2fd18b762
Aleksandar Gajic <ale.gajic92@gmail.com>
2023-08-09 13:01:54 +0200

    Navigation transition

--- app/src/main/java/com/acszo/redomi/ui/nav/RootNavigation.kt
+++ app/src/main/java/com/acszo/redomi/ui/nav/RootNavigation.kt
@@ -1,6 +1,18 @@
 package com.acszo.redomi.ui.nav
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,6 +22,8 @@ import com.acszo.redomi.ui.settings.AppsPage
 import com.acszo.redomi.ui.settings.LayoutPage
 import com.acszo.redomi.ui.settings.SettingsPage
 
+const val initialOffset = 0.10f
+
 @Composable
 fun RootNavigation() {
     val navController: NavHostController = rememberNavController()
@@ -18,19 +32,68 @@ fun RootNavigation() {
         navController = navController,
         startDestination = Route.SettingsPage.route,
     ) {
-        composable(route = Route.SettingsPage.route) {
+        navigationComposable(
+            route = Route.SettingsPage.route
+        ) {
             SettingsPage(navController = navController)
         }
-        composable(route = Route.AppsPage.route) {
+        navigationComposable(
+            route = Route.AppsPage.route
+        ) {
             AppsPage {
                 BackButton { navController.popBackStack() }
             }
         }
-        composable(route = Route.LayoutPage.route) {
+        navigationComposable(
+            route = Route.LayoutPage.route
+        ) {
             LayoutPage {
                 BackButton { navController.popBackStack() }
             }
         }
     }
 
+}
+
+fun NavGraphBuilder.navigationComposable(
+    route: String,
+    content: @Composable (AnimatedContentScope.(NavBackStackEntry) -> Unit)
+) = composable(
+    route = route,
+    enterTransition = { enterTransition(initialOffset) },
+    exitTransition = { exitTransition(-initialOffset) },
+    popEnterTransition = { enterTransition(-initialOffset) },
+    popExitTransition = { exitTransition(initialOffset) },
+    content = content,
+)
+
+fun enterTransition(
+    initialOffset: Float
+): EnterTransition {
+    return fadeIn(
+        animationSpec = tween(
+            durationMillis = 210,
+            delayMillis = 90,
+            easing = LinearOutSlowInEasing
+        )
+    ) + slideInHorizontally(
+        animationSpec = tween(durationMillis = 300)
+    ) {
+        (it * initialOffset).toInt()
+    }
+}
+
+fun exitTransition(
+    targetOffset: Float
+): ExitTransition {
+    return fadeOut(
+        animationSpec = tween(
+            durationMillis = 90,
+            easing = FastOutLinearInEasing
+        )
+    ) + slideOutHorizontally(
+        animationSpec = tween(durationMillis = 300)
+    ) {
+        (it * targetOffset).toInt()
+    }
 }
