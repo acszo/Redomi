49227a60297d3ffbc76e06055adb7cda997cad24
ale.gajic <ale.gajic92@gmail.com>
2023-09-11 15:55:22 +0200

    Updated non dynamic color scheme

--- app/src/main/java/com/acszo/redomi/ui/theme/Color.kt
+++ app/src/main/java/com/acszo/redomi/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.acszo.redomi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryDark = Color(0xFF0061a5)
+val SecondaryDark = Color(0xFF535f70)
+val TertiaryDark = Color(0xFF6b5778)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryLight = Color(0xFF9fcaff)
+val SecondaryLight = Color(0xFFbbc7db)
+val TertiaryLight = Color(0xFFd7bee4)
--- app/src/main/java/com/acszo/redomi/ui/theme/Theme.kt
+++ app/src/main/java/com/acszo/redomi/ui/theme/Theme.kt
@@ -17,25 +17,15 @@ import androidx.core.view.WindowCompat
 import com.acszo.redomi.MainActivity
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryLight,
+    secondary = SecondaryLight,
+    tertiary = TertiaryLight
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryDark,
+    secondary = SecondaryDark,
+    tertiary = TertiaryDark
 )
 
 @Composable
