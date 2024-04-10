6b7d049380c401a86c7d704059507b0c5231aa7c
ale.gajic <ale.gajic92@gmail.com>
2024-04-10 10:16:11 +0200

    delete apk

--- app/src/main/java/com/acszo/redomi/MainActivity.kt
+++ app/src/main/java/com/acszo/redomi/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.acszo.redomi.ui.nav.RootNavigation
 import com.acszo.redomi.ui.theme.RedomiTheme
+import com.acszo.redomi.utils.UpdateUtil.deleteApk
 import com.acszo.redomi.viewmodel.DataStoreViewModel
 import com.acszo.redomi.viewmodel.UpdateViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,6 +30,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
 
         setContent {
+            val context = LocalContext.current
+            val scope = rememberCoroutineScope()
+
             val dataStoreViewModel: DataStoreViewModel = hiltViewModel()
             val updateViewModel: UpdateViewModel = hiltViewModel()
             val versionName = BuildConfig.VERSION_NAME
@@ -35,6 +43,9 @@ class MainActivity : ComponentActivity() {
                 dataStoreViewModel.getLayoutListType()
                 dataStoreViewModel.getLayoutGridSize()
                 dataStoreViewModel.getThemeMode()
+                scope.launch(Dispatchers.IO) {
+                    deleteApk(context)
+                }
             }
 
             val isUpdateAvailable by updateViewModel.isUpdateAvailable.collectAsStateWithLifecycle()
