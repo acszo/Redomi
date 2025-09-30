8100d79a87cb63fc00d2010422cf0170d9e85726
ale.gajic <ale.gajic92@gmail.com>
2025-09-30 11:36:46 +0200

    sanitize url, fixes #37, closes #39

--- app/src/main/java/com/acszo/redomi/ConvertSongActivity.kt
+++ app/src/main/java/com/acszo/redomi/ConvertSongActivity.kt
@@ -23,19 +23,23 @@ import dagger.hilt.android.AndroidEntryPoint
 * ACTION_VIEW -> shows opening apps -> opens app
 * */
 @AndroidEntryPoint
-class ConvertSongActivity: ComponentActivity() {
+class ConvertSongActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val isActionSend = (intent.action == Intent.ACTION_SEND)
+        var intentData: String
+        var appList = AppList.OPENING
+
         setContent {
-            val isActionSend = (intent.action == Intent.ACTION_SEND)
-
-            val intentData = intent?.let {
-                if (isActionSend) it.getStringExtra(Intent.EXTRA_TEXT) else it.data
-            }.toString()
-
-            val appList = if (isActionSend) AppList.SHARING else AppList.OPENING
+            if (isActionSend) {
+                intentData = intent?.getStringExtra(Intent.EXTRA_TEXT)
+                    ?.replaceFirst(".*(?=https://)".toRegex(), "").toString()
+                appList = AppList.SHARING
+            } else {
+                intentData = intent?.data.toString()
+            }
 
             val songLinkViewModel: SongLinkViewModel = hiltViewModel()
             val updateViewModel: UpdateViewModel = hiltViewModel()
