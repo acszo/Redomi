103abd3f4565b899ed019fa90965f6bd851df07a
ale.gajic <ale.gajic92@gmail.com>
2025-11-09 10:29:20 +0100

    refactor

--- app/src/main/java/com/acszo/redomi/ui/nav/RootNavigation.kt
+++ app/src/main/java/com/acszo/redomi/ui/nav/RootNavigation.kt
@@ -1,20 +1,17 @@
 package com.acszo.redomi.ui.nav
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.acszo.redomi.isGithubBuild
+import com.acszo.redomi.ui.common.BackButton
 import com.acszo.redomi.ui.common.TransitionDirection
 import com.acszo.redomi.ui.common.enterHorizontalTransition
 import com.acszo.redomi.ui.common.exitHorizontalTransition
-import com.acszo.redomi.ui.common.BackButton
+import com.acszo.redomi.ui.page.settings.SettingsPage
 import com.acszo.redomi.ui.page.settings.apps.AppsPage
 import com.acszo.redomi.ui.page.settings.layout.LayoutPage
-import com.acszo.redomi.ui.page.settings.SettingsPage
 import com.acszo.redomi.ui.page.settings.update.UpdatePage
 import com.acszo.redomi.viewmodel.DataStoreViewModel
 import com.acszo.redomi.viewmodel.UpdateViewModel
@@ -30,8 +27,12 @@ fun RootNavigation(
     NavHost(
         navController = navController,
         startDestination = Route.SettingsPage.route,
+        enterTransition = { enterHorizontalTransition(TransitionDirection.FORWARD) },
+        exitTransition = { exitHorizontalTransition(TransitionDirection.BACKWARD) },
+        popEnterTransition = { enterHorizontalTransition(TransitionDirection.BACKWARD) },
+        popExitTransition = { exitHorizontalTransition(TransitionDirection.FORWARD) },
     ) {
-        navigationComposable(
+        composable(
             route = Route.SettingsPage.route
         ) {
             SettingsPage(
@@ -40,7 +41,7 @@ fun RootNavigation(
                 isUpdateAvailable = isUpdateAvailable
             )
         }
-        navigationComposable(
+        composable(
             route = Route.AppsPage.route
         ) {
             AppsPage(
@@ -49,7 +50,7 @@ fun RootNavigation(
                 BackButton { navController.popBackStack() }
             }
         }
-        navigationComposable(
+        composable(
             route = Route.LayoutPage.route
         ) {
             LayoutPage(
@@ -60,7 +61,7 @@ fun RootNavigation(
         }
 
         if (isGithubBuild) {
-            navigationComposable(
+            composable(
                 route = Route.UpdatePage.route
             ) {
                 UpdatePage(
@@ -73,15 +74,3 @@ fun RootNavigation(
     }
 
 }
-
-fun NavGraphBuilder.navigationComposable(
-    route: String,
-    content: @Composable (AnimatedContentScope.(NavBackStackEntry) -> Unit)
-) = composable(
-    route = route,
-    enterTransition = { enterHorizontalTransition(TransitionDirection.FORWARD) },
-    exitTransition = { exitHorizontalTransition(TransitionDirection.BACKWARD) },
-    popEnterTransition = { enterHorizontalTransition(TransitionDirection.BACKWARD) },
-    popExitTransition = { exitHorizontalTransition(TransitionDirection.FORWARD) },
-    content = content,
-)
