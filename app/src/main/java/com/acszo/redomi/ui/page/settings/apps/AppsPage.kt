06200a87b32913d1d212fd9d72b8eca73d53d038
acszo <ale.gajic92@gmail.com>
2026-05-22 19:08:18 +0200

    rememberSavable for selected tab

--- app/src/main/java/com/acszo/redomi/ui/page/settings/apps/AppsPage.kt
+++ app/src/main/java/com/acszo/redomi/ui/page/settings/apps/AppsPage.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -34,7 +34,7 @@ fun AppsPage(
     dataStoreViewModel: DataStoreViewModel,
     backButton: @Composable () -> Unit
 ) {
-    val selectedTab = remember { mutableStateOf(AppList.OPENING) }
+    val selectedTab = rememberSaveable { mutableStateOf(AppList.OPENING) }
 
     LaunchedEffect(Unit) {
         dataStoreViewModel.getOpeningApps()
